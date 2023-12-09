c76156d6140aaf0332577fc36552bab38238c2d1
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-09 22:30:15 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-020_Tilt_Switch/KY-020_Tilt_Switch.ino
@@ -0,0 +1,22 @@
+int Led = 13 ;// define LED Interface
+int tiltpin = 2; // define the tilt switch pin
+int val ;// define numeric variables val
+
+void setup ()
+{
+  pinMode (Led, OUTPUT) ;// define LED as output interface
+  pinMode (tiltpin, INPUT) ;//define the output interface tilt switch sensor
+}
+
+void loop ()
+{
+  val = digitalRead (tiltpin) ;// read the pin
+  if (val == HIGH) //When the tilt sensor detects a signal when the switch, LED flashes 
+  {
+    digitalWrite (Led, HIGH);
+  }
+  else
+  {
+    digitalWrite (Led, LOW);
+  }
+}
