9014a8a95ae83dae6fc87d5448d0bb815a48f2c1
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-09 23:15:02 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-018_PhotoResistor/KY-018_PhotoResistor.ino
@@ -0,0 +1,26 @@
+int sensorPin = A1; // Declare the input pin here
+ 
+void setup()
+{
+    Serial.begin(9600);
+}
+ 
+void loop()
+{      
+    // Current voltage value is measured...
+    int rawValue = analogRead(sensorPin);
+    float voltage = rawValue * (5.0/1023) * 1000;
+    float resitance = 10000 * ( voltage / ( 5000.0 - voltage) );
+    
+    Serial.print("Voltage value:"); 
+    Serial.print(voltage); 
+    Serial.print(" mV");
+    Serial.println("");
+    Serial.print("resistance value:"); 
+    Serial.print(resitance); 
+    Serial.print(" Ohms");
+    Serial.println("");
+    Serial.println("---------------------------------------");
+ 
+    delay(500);
+}
