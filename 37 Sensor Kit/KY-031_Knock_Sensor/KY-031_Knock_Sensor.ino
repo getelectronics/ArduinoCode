2043aa94dd4e1335e7bfdf14b0ce3357297889a5
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-09 22:46:18 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-031_Knock_Sensor/KY-031_Knock_Sensor.ino
@@ -0,0 +1,23 @@
+int LedPin = 13 ;// define LED Interface
+int ShockPin = 2; // define the percussion Sensor Interface
+int val ;// define numeric variables val
+
+void setup ()
+{
+  pinMode (LedPin, OUTPUT) ;// define LED as output interface
+  pinMode (ShockPin, INPUT) ;// define knock sensor output interface
+}
+
+void loop ()
+{
+  val = digitalRead (ShockPin) ;// read ShockPin
+  if (val == HIGH) // When the knock switch is triggered the LED flashes
+  {
+    digitalWrite (LedPin, LOW);
+  }
+  else
+  {
+    digitalWrite (LedPin, HIGH);
+  }
+}
+
