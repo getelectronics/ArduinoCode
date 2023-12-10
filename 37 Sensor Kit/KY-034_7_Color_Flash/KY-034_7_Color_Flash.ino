d9ec58a254cb9abd6b1a38d6247d73344fa5f2ae
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-10 12:40:35 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-034_7_Color_Flash/KY-034_7_Color_Flash.ino
@@ -0,0 +1,14 @@
+int ledPin = 11;    // pin to control KY-034
+
+void setup() 
+{                
+  pinMode(ledPin, OUTPUT);    // define pin as output
+}
+
+void loop() 
+{
+  digitalWrite(ledPin, HIGH); // turn LED on
+  delay(3000);                // wait for 3 seconds
+  digitalWrite(ledPin, LOW);  // turn LED off
+  delay(1000);                // wait for a second
+}
