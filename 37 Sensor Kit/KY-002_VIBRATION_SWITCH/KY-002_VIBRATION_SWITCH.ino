ad5a706eb1315292e80bf5a7066d7418d970849a
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-10 10:17:09 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-002_VIBRATION_SWITCH/KY-002_VIBRATION_SWITCH.ino
@@ -0,0 +1,34 @@
+int shockPin = 10; // sensor connected to pin 10
+int shockVal = HIGH;
+boolean bAlarm = false;
+unsigned long lastShockTime;
+int shockAlarmTime = 250; // Number of milli seconds to keep the shock alarm high
+
+void setup ()
+{
+  Serial.begin(9600);  
+  pinMode (shockPin, INPUT)
+}
+
+void loop ()
+{
+  shockVal = digitalRead (shockPin) ; // read the value from the sensor
+  
+  if (shockVal == LOW)
+  {
+    lastShockTime = millis();
+    if (!bAlarm)
+    {
+      Serial.println("Shock detected");
+      bAlarm = true;
+    }
+  }
+  else
+  {
+    if( (millis()-lastShockTime) > shockAlarmTime  &&  bAlarm)
+    {
+      Serial.println("No Shock detected");
+      bAlarm = false;
+    }
+  }
+}
