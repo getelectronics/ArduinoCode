8befde59ad44ea70893e6d6094761f9a655d3a10
getelectronics <30749112+getelectronics@users.noreply.github.com>
2023-12-10 11:18:21 +0000

    Add files via upload

---
+++ 37 Sensor Kit/KY-029_2_Color/KY-029_2_Color.ino
@@ -0,0 +1,20 @@
+int redLED = 11;
+int greenLED = 10;
+ 
+void setup ()
+{
+  // Initialize output pins for the LEDs
+  pinMode (redLED, OUTPUT); 
+  pinMode (greenLED, OUTPUT); 
+}
+ 
+void loop ()
+{
+  digitalWrite (redLED, HIGH); // LED is switched on
+  digitalWrite (greenLED, LOW); // LED is switched off
+  delay (2000);
+ 
+  digitalWrite (redLED, LOW); // LED is switched off
+  digitalWrite (greenLED, HIGH); // LED is switched on
+  delay (2000);
+}
